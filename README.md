29c9aac36e0b2db01c8ed8074633bd8c695cc6ae
jixiaolong01 <jixiaolong01@baidu.com>
2019-04-19 16:13:00 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+## Install Environment
+    bash /home/longriyao/sdb/test/first_version/build.sh
+    
+    
+## Run
+
+    conda activate pyqt4_python3
+    python pyqtChatApp.py
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-conda create --name pyqt4_python5 python=3.5 pyqt=4.11.3
-pip install chatterbot
+conda create --name pyqt4_python3 python=3.5 pyqt=4.11.3
+# pip install chatterbot
+conda install pytorch=1.0.0
 pip install pytz
 pip install vtk
